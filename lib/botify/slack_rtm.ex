27829095f51447c3b8fd64b917e267a0e9516907
Jackson Popkin <jackson@donorschoose.org>
2017-01-09 23:26:50 -0500

    Launch basic Slackbot at startup

---
+++ lib/botify/slack_rtm.ex
@@ -0,0 +1,28 @@
+defmodule Botify.SlackRtm do
+  use Slack
+
+  def handle_connect(slack, state) do
+    IO.puts "Connected as #{slack.me.name}"
+    {:ok, state}
+  end
+
+  def handle_event(message = %{type: "message"}, slack, state) do
+    target_channel = System.get_env("SLACK_CHANNEL")
+    case message.channel do
+      ^target_channel -> send_message("I got a message!", message.channel, slack)
+      _ -> nil
+    end
+
+    {:ok, state}
+  end
+  def handle_event(_, _, state), do: {:ok, state}
+
+  def handle_info({:message, text, channel}, slack, state) do
+    IO.puts "Sending your message, captain!"
+
+    send_message(text, channel, slack)
+
+    {:ok, state}
+  end
+  def handle_info(_, _, state), do: {:ok, state}
+end
