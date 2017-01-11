b17081b2ab5e61a89717d1c82b4993b3a7f3216b
Jackson Popkin <jackson@donorschoose.org>
2017-01-10 22:50:03 -0500

    React to Spotify links

--- lib/botify/slack_rtm.ex
+++ lib/botify/slack_rtm.ex
@@ -9,13 +9,26 @@ defmodule Botify.SlackRtm do
   def handle_event(message = %{type: "message"}, slack, state) do
     target_channel = System.get_env("SLACK_CHANNEL")
     case message.channel do
-      ^target_channel -> send_message("I got a message!", message.channel, slack)
+      ^target_channel -> handle_message(message, slack)
       _ -> nil
     end
 
     {:ok, state}
   end
   def handle_event(_, _, state), do: {:ok, state}
+
+  defp handle_message(message = %{text: _}, _slack) do
+    is_track_link = Regex.match?(~r'https://(open|play).spotify.com/track/', message.text)
+
+    if is_track_link do
+      options = %{channel: message.channel, timestamp: message.ts, token: System.get_env("SLACK_TOKEN")}
+      Slack.Web.Reactions.add('musical_note', options)
+      nil
+    end
+  end
+  defp handle_message(_, _) do
+    nil
+  end
 
   def handle_info({:message, text, channel}, slack, state) do
     IO.puts "Sending your message, captain!"
