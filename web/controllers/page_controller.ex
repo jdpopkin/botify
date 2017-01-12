d4c6db2eb854f27a1d5c74fc7bb34102108f1e33
Jackson Popkin <jackson@donorschoose.org>
2017-01-11 22:19:05 -0500

    Link to Spotify authorization

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule Botify.PageController do
   use Botify.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render conn, "index.html", authorization_url: Spotify.Authorization.url
   end
 end
